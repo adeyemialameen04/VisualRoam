a10999c834550ba96f89a12b45352517a810de1f
Al Ameen Adeyemi <adeyemialameen04@gmail.com>
2023-08-17 13:31:59 +0100

    configred react-query and react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Navbar from "./Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar />
       <h1>Hello</h1>
-    </>
+    </Router>
   );
 }
 
